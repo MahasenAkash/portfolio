276ed0d0ce8fd453b80293740f8272d364ae177d
A.M. Vidanage <mahasenakash4@gmail.com>
2026-05-19 01:19:10 +0530

    build professional navbar and layout structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
+import Navbar from "./components/Navbar";
 export default function Home() {
   return (
-    <main className="min-h-screen bg-slate-950 text-white flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-5xl font-bold">
-          Akash Vidanage
-        </h1>
+    <>
+      <Navbar />
+      <main className="min-h-screen bg-slate-950 text-white pt-20 flex items-center justify-center">
+        <div className="text-center px-4">
+          <h1 className="text-5xl font-bold">Akash Vidanage</h1>
 
-        <p className="text-slate-400 mt-4">
-          Computer Science Student
-        </p>
-      </div>
-    </main>
+          <p className="text-slate-400 mt-4">Computer Science Student</p>
+        </div>
+      </main>
+    </>
   );
 }
